7e88ed0434f8e1fb7d5ea3215a4c53b76828336e
KARTHIK LAGVANKAR <77400421+LagvankarKarthik@users.noreply.github.com>
2023-09-21 23:41:02 +0530

    Add files via upload

---
+++ Madlibs_Game.py
@@ -0,0 +1,7 @@
+colour = input("Enter a colour: ")
+plural_noun = input("Enter a Plural Noun: ")
+celebrity = input("Enter s celebrity: ")
+
+print("Roses are " + colour)
+print(plural_noun + " are blue")
+print(celebrity + " I love you!")
