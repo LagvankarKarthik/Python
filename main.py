f57571145984234b7222537bd2f8b071030964b2
KARTHIK LAGVANKAR <77400421+LagvankarKarthik@users.noreply.github.com>
2024-01-03 17:52:14 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
-num1 = input("Enter  a number: ")
-num2 = input("Enter another number: ")
-operator = input("Enter an operator: ")
+#Any type of cariables can be put in the lists. Python is going to be fine with it.
+friends = ['Rolf', 'Bob', 'Jen', 'Anne', 'Jim', 'Joe']
+lucky_numbers = [4, 8, 15, 16, 23, 42]
+print(friends)
+print(friends[2:5])
 
-if operator == '+':
-  result = float(num1) + float(num2)
+#list functions
+friends.append('Swathi')  #appending another item in the end of the kist
+print(friends)
+#insert function that takes two parameters, first is index and name of the element
+friends.insert(13, 'Karthik')
+print(friends)
+friends.remove('Jim')
+print(friends)
+print(friends.index('Swathi'))  #To check if a certain eleent is in the list
+print(friends.count(
+    'Swathi'))  #Will tell us how many times the element is in the list
+friends.sort()  #To sort the list in asccending order
+print(friends)
+friends.reverse()  #To reverse the list
+print(friends)
 
-if operator == '-':
-  result = float(num1) - float(num2)
+friends.extend(lucky_numbers) #adding 2 lists together
+print(friends) 
+friends2 = friends.copy() #Friends2 has all the attributes of the friends list.
+print(friends2)
 
-if operator == '*':
-  result = float(num1) * float(num2)
 
-if operator == '/':
-  result = float(num1) / float(num2)
 
-print(result)
+friends.pop()  # To get rid of the last element in the array.
+print(friends)
+friends.clear() #To get rid of everything in the list
+print(friends) 
